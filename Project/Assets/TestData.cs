f3ca02376002988b2bfa2dc32d847be2888e4a93
Nguyen Nhat Long <nhatlongcode@gmail.com>
2021-11-14 03:14:05 +0700

    Convert id bigint and bits

--- Project/Assets/TestData.cs
+++ Project/Assets/TestData.cs
@@ -8,24 +8,29 @@ public class TestData : MonoBehaviour
 
     public string idString;
     public ID id = new ID();
-    private void Start() {
+    private void Start() 
+    {
         var result = new System.Text.StringBuilder();
         id.bigInt = BigInteger.Parse(idString);
-        
-        foreach(var bite in id.bytes)
-        {
-            var bits = new BitArray(bite);
-            result.Append(ToBitString(bits));
-        }
-        result.ToString();
-        Debug.Log(result);
+        var bits = new BitArray(id.bytes);
+        Debug.Log(ToBitString(bits));
+        // foreach(var bite in id.bytes)
+        // {
+        //     var bits = new BitArray()
+        //     //result.Append(ToBitString(bits));
+        //     //Debug.Log(ToBitString(bits));
+        //     Debug.Log(bite);
+        //     Debug.Log(ToBitString(bits))
+        // }
+        //result.ToString();
+        //Debug.Log(result);
     }
 
     public string ToBitString(BitArray bits)
     {
         var sb = new System.Text.StringBuilder();
 
-        for (int i = 0; i < bits.Count; i++)
+        for (int i = bits.Count - 1; i >= 0; i--)
         {
             char c = bits[i] ? '1' : '0';
             sb.Append(c);
